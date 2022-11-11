cfe782730ea8aaaadfe5c431601e8782853109f3
Schleidens <alectineschleidens2016@gmail.com>
2022-11-11 16:02:38 -0500

    setup the calculator skeleton

--- src/components/layout/Calculator.js
+++ src/components/layout/Calculator.js
@@ -3,7 +3,39 @@ import React from 'react'
 function calculator() {
   return (
     <div className='calculator__layout'>
-        i'm the calculator
+       <table className='the__calculator'>
+            <tr>
+              <th colSpan="4">result</th>
+            </tr>
+            <tr>
+              <td>9</td>
+              <td>8</td>
+              <td>7</td>
+              <td>/</td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>5</td>
+              <td>4</td>
+              <td>*</td>
+            </tr>
+
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>-</td>
+            </tr>
+
+            <tr>
+              <td colSpan="2">0</td>
+              <td>.</td>
+              <td>+</td>
+            </tr>
+            <tr>
+              <td colSpan="4">=</td>
+            </tr>
+      </table>
     </div>
   )
 }
