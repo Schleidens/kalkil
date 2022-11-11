683a75023e12152f99891fe5d51c14b568a3e853
Schleidens <alectineschleidens2016@gmail.com>
2022-11-11 15:28:09 -0500

    setup homepage and skelton

---
+++ src/components/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div className='main'>
+       <div className='calculator'>
+            calculator
+        </div>
+       <div className='pose'>
+            pose
+       </div>
+    </div>
+  )
+}
+
+export default Homepage
