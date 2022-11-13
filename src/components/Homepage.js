2e796c153cbea5e1dce970a59a3d3d6f7c9bf9a6
Schleidens <alectineschleidens2016@gmail.com>
2022-11-13 15:15:24 -0500

    add and design the right section and make it responsive

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Calculator from './layout/Calculator'
+import Pose from './layout/Components/Pose'
 
 function Homepage() {
   return (
@@ -8,7 +9,24 @@ function Homepage() {
             <Calculator />
         </div>
        <div className='pose'>
-            pose
+            <div className="pose__box">
+
+              {/* the title */}
+              <h1 className='pose__title'>
+                KALKIL
+              </h1>
+
+              {/* the subtitle */}
+              <p className='pose__subtitle'>
+                A web based calculator app create by <a href="https://github.com/Schleidens" target="_blank" rel="noreferrer">Schleidens.Dev</a>
+                ,
+                <br /> 
+                (this app will not have any future maintain as it was a learning project) :)
+              </p>
+
+                {/* pose components */}
+              <Pose />
+            </div>
        </div>
     </div>
   )
